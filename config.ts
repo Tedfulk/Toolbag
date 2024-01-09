cca307cf103ac045863064bfbcbdf11fbadba35c
Ted Fulk <tfulk@getspiffy.com>
2024-01-09 18:44:08 -0500

    refactor: removed the gpt crawler project

--- config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from "./src/config";
-
-export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 3,
-  outputFileName: "prompt_templates/GPTs/knowledge_base/output.json", // change output to a more descriptive name for what you are crawling
-};
