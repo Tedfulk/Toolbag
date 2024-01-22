a98524e85ab277a6b59c1d3152550f705e3a906d
Tedfulk <teddyfulk@gmail.com>
2024-01-22 09:41:56 -0500

    Update dependencies and configuration

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+import asyncio
 from tedfulk_kb_pycrawler import scrape_it, Config
 
-# import asyncio
 
 config = Config(
     urls=[
         "https://jxnl.github.io/instructor/",
-        "https://turso.tech/",
     ],
-    output_file_name=["instructor", "turso"],  # default is "output"
-    max_pages_to_crawl=2,  # default is 10
+    output_file_name=["instructor"],
+    output_file_type=["pdf"],
+    max_pages_to_crawl=100,
 )
-
-scrape_it(config)
+asyncio.run(scrape_it(config))
